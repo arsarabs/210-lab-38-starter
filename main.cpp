084f5f41c60e254c6d1526e93563e147c8681573
Your Name <your-email@example.com>
2024-12-01 03:32:43 -0800

    setting up main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
+
 #include <iostream>
+#include <fstream>
 #include "StringBinaryTree.h"
-using namespace std;
+
+const string FILENAME = "codes.txt";
+
+//Prototypes here
+//need some basic functions for displaying menu, gets userChoice, and gets userString
+void displayMenu();
+int userChoice();
+string userString(const string &prompt);
+
 
 int main() {
     //starting here 
 
     return 0;
+}
+
+//Definitions
+void displayMenu() {
+
+}
+int userChoice() {
+
+}
+string userString(const string& prompt) {
+
 }
